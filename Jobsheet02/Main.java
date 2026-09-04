937f16eab69e24d3bf82129a8ba669d814c369f8
AliPasha <ahmdrzlap327@gmail.com>
2026-09-04 09:20:47 +0700

    checkpoint 5

--- Jobsheet02/Main.java
+++ Jobsheet02/Main.java
@@ -2,12 +2,14 @@ package Jobsheet02;
 
 public class Main {
     public static void main(String[] args) {
-        account acc = new account("Nadia", 500000);
-        acc.withdraw(150000);
-        acc.printInfo();
-        System.out.println("Formatted: " + acc.formatBalance());
+        account original     = new account("Nadia", 500000);
+        account copy = original;
+        copy.deposit(100000);
 
-        acc.withdraw(1000000);
-        acc.printInfo();
+        System.out.println("Via original: " + original.balance);
+        System.out.println("Via copy: " + copy.balance);
+
+        account empty = null;
+        empty.printInfo();
     }
 }
