983c5eb4d07d2a1df92c79f65d2df123badb6729
AliPasha <ahmdrzlap327@gmail.com>
2026-09-20 18:32:02 +0700

    tugas

--- Jobsheet04/Main.java
+++ Jobsheet04/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Customer customer1 = new Customer("Nadia", "0812-0000-0001");
         Account acc1 = new Account("A001", customer1, 500000);
+        Account acc3 = new Account("A003", customer1, 1500000);
         acc1.withdraw(150000);
 
         Customer customer2 = new Customer("Sari", "0812-0000-0002");
@@ -12,11 +13,13 @@ public class Main {
         Bank bank = new Bank(10);
         bank.addAccount(acc1);
         bank.addAccount(acc2);
+        bank.addAccount(acc3);
         bank.printAllAccounts();
 
-        Account found = bank.findAccount("A002");
-        if (found != null) {
-            found.printInfo();
+        Account[] nadiaAccounts = bank.findAccountsByOwnerName("Nadia");
+        System.out.println("Jumlah rekening atas nama Nadia: " + nadiaAccounts.length);
+        for (int i = 0; i < nadiaAccounts.length; i++) {
+            nadiaAccounts[i].printInfo();
         }
     }
 }
